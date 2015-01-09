3317d20d3b587b6e2f2c8b86ad65495168091088
Devon Truman <dtruman@student.neumont.edu>
2015-01-09 17:17:35 +0000

    the Item prefab has an ItemInteraction script attached to it. The ItemInteraction script allows objects to be picked up by the player using the Space Key if they are close enough to the object and are looking at it. Items must have the tag "Item" on them for them to be treated as Items. You can read an item by pressing the R key when holding the item or looking at it. You can throw the item by holding down the F key and releasing the Space key. The readable text can be edited on the objects script, it is labeled "info".
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@8 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/AIDetection.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIDetection : MonoBehaviour
+{
+
+    private bool withinRange = false;
+    private bool recentlyDetected = false;
+    private Vector3 playerDirection;
+    public float viewAngle = 90;
+    public float viewDistance = 8;
+    public float speed = 0.4f;
+    public float exploreCoordX=0;
+    public float exploreCoordZ=0;
+    public float exploreDistance = 20;
+    private Vector2 exploreNode;
+    // Use this for initialization
+    void Start()
+    {
+        SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+        myCollider.radius = viewDistance;
+
+        exploreNode.x = Random.Range(exploreCoordX, exploreCoordX + exploreDistance);
+        exploreNode.y = Random.Range(exploreCoordZ, exploreCoordZ + exploreDistance);
+
+        float angle = Vector3.Angle(new Vector3(exploreNode.x, transform.position.y, exploreNode.y), transform.forward);
+
+        transform.RotateAround(transform.position, transform.up, angle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject playerObject = GameObject.Find("Player");
+
+        if (withinRange)
+        {
+            Vector3 player = playerObject.rigidbody.position;
+            playerDirection = player - transform.position;
+            float angle = Vector3.Angle(playerDirection, transform.forward);
+            if (angle < (viewAngle * 0.5f))
+            {
+                RaycastHit hitOne;
+
+                SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+
+                if (Physics.Raycast(transform.position + transform.up, playerDirection.normalized, out hitOne, myCollider.radius))
+                {
+                    if (hitOne.collider.gameObject == playerObject)
+                    {
+                        transform.LookAt(new Vector3(player.x, transform.position.y, player.z));
+                    }
+                }
+            }
+            else
+            {
+                recentlyDetected = false;
+            }
+        }
+
+        if (!recentlyDetected)
+        {
+            if (Mathf.Abs(exploreNode.x-transform.position.x) < 1.0f && Mathf.Abs(exploreNode.y-transform.position.z) < 1.0f)
+            {
+                exploreNode.x = Random.Range(exploreCoordX, exploreCoordX + exploreDistance);
+                exploreNode.y = Random.Range(exploreCoordZ, exploreCoordZ + exploreDistance);
+
+                transform.LookAt(new Vector3(exploreNode.x, transform.position.y, exploreNode.y));
+            }
+            Vector3 pd = (new Vector3(exploreNode.x, transform.position.y, exploreNode.y))-(transform.position);
+            pd = pd.normalized * speed;
+            transform.Translate(pd.x, 0.0f, pd.z);
+        }
+        else if (recentlyDetected)
+        {
+            exploreCoordX=playerObject.rigidbody.position.x;
+            exploreCoordZ=playerObject.rigidbody.position.z;
+            Vector3 pd = (transform.position) - (playerObject.rigidbody.position);
+            pd = pd.normalized * speed;
+            transform.Translate(pd.x, 0.0f, pd.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            withinRange = true;
+        }
+        else
+            withinRange = false;
+    }
+}
---
+++ SpoopyGame/Assets/Scripts/ItemInteraction.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemInteraction : MonoBehaviour {
+
+    public string info = "Just an object";
+    private float grabDistance = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject playerHead = GameObject.Find("Head");
+
+        RaycastHit hitOne;
+
+        if (Physics.Raycast(playerHead.transform.position, playerHead.transform.forward.normalized, out hitOne) && (transform.position-playerHead.transform.position).magnitude<grabDistance)
+        {
+            if (hitOne.collider.gameObject.tag == "Item")
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    transform.position = playerHead.transform.position + (playerHead.transform.forward.normalized * 2);
+                    transform.rigidbody.useGravity = false;
+                }
+                else
+                    transform.rigidbody.useGravity = true;
+
+                if (Input.GetKey(KeyCode.F))
+                {
+                    if (Input.GetKey(KeyCode.Space))
+                    { }
+                    else
+                    {
+                        transform.rigidbody.AddForce(playerHead.transform.forward.normalized * 3);
+                    }
+                }
+            }
+        }
+	}
+
+    void OnGUI()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            GUIStyle GUIS = new GUIStyle();
+            GUIS.alignment = TextAnchor.MiddleCenter;
+            GUIS.normal.textColor = Color.white;
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 0, 0), info, GUIS);
+        }
+    }
+}
