199580f25c28eb299116ea1fee97a7c003e8a77c
Devon Truman <dtruman@student.neumont.edu>
2015-01-09 16:13:43 +0000

    Player Prefab is changed to allow collision with objects. Enemy Prefab is created, use this (change the mesh if needed) for enemies. Currently enemies only respond if they see you, the enemy has a script called AIDetection, currently it has a View Angle (the angle of view the enemy has), a View Distance (How far the enemy can see), a Speed (how fast the enemy can move), a Explore Coord X and Z (this defines the starting position to where the enemy will explore), and an Explore Distance (which defines how great of an area the enemy will explore). If an enemy sees a player it will chase you, if you escape its line of sight (walk behind an object) it will use the last known location of you as its new Explore Coord. Things to think about for future: Choosing exploration positions does not take into account walls, adding a raycast to check if it can reach a point would satisfy this issue.
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@5 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/SimplePlayerTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            rigidbody.AddForce(Vector3.forward * 20, ForceMode.Force);
+        }
+	}
+}
