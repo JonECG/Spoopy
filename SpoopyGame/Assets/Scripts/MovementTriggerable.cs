22444a3f6ff9f75e2688c7da871eaf08408ba16d
Anson Wayman <Kanoyugoro@gmail.com>
2015-01-12 17:15:17 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@32 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/MovementTriggerable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementTriggerable : Triggerable
+{
+    private Vector3 startPosition;
+    public Vector3 endPosition;
+    public float speedMultiplier;
+    private bool moving = false;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (moving)
+        {
+            startPosition = this.transform.position;
+            transform.LookAt(endPosition);
+
+            this.transform.Translate(transform.forward.normalized * speedMultiplier * Time.deltaTime);
+
+            if ((transform.position - endPosition).magnitude < 1.0f)
+            {
+                //dissapear
+                moving = false;
+            }
+
+        }
+	}
+
+    public void Triggered(string id)
+    {
+        moving = true;
+    }
+}
---
+++ SpoopyGame/Assets/Scripts/SoundTriggerable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerable : Triggerable 
+{
+    public AudioClip soundToPlay;
+    public Vector3 positionToPlayAt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Triggered(string id)
+    {
+        SoundManagerController.Instance.PlaySoundAt(soundToPlay, positionToPlayAt);
+    }
+}
