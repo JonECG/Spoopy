9f47beca767e2f32aec0f0ec902312123c05e497
Talan Lousignont <Talan.L.Lousignont@gmail.com>
2015-01-12 06:47:49 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@19 7a713aaa-3320-a543-9002-538bd3c5cc84

--- SpoopyGame/Assets/Scripts/HeadLamp.cs
+++ SpoopyGame/Assets/Scripts/HeadLamp.cs
@@ -7,11 +7,13 @@ public class HeadLamp : MonoBehaviour {
     public float batteryLifeInSeconds = 60;
     public float numberOfShakesToCharge = 6;
     public float weakenTime = 5;
+    public float debounceThreshhold = 0.1f;
 
+    private float debounce = 0.1f;
     private float currentBatteryLife;
     private bool isTurnedOn;
     private float maxIntensity;
-
+    private bool debouncing = false;
 	void Start () 
 	{
         isTurnedOn = false;
@@ -21,11 +23,34 @@ public class HeadLamp : MonoBehaviour {
 	
 	void Update () 
 	{
-        if (Input.GetAxis("HeadLampToggle") > 0.0f)
+
+        if (debounce >= debounceThreshhold || !debouncing)
         {
-            SoundManagerController.Instance.PlaySoundAt(isTurnedOn ? clickOff : (currentBatteryLife > 0) ? clickOn : clickDead, transform.position);
-            if (currentBatteryLife > 0)
-                isTurnedOn = !isTurnedOn;
+            debouncing = false;
+            if (Input.GetAxis("HeadLampToggle") > 0.0f)
+            {
+                SoundManagerController.Instance.PlaySoundAt(isTurnedOn ? clickOff : (currentBatteryLife > 0) ? clickOn : clickDead, transform.position);
+                if (currentBatteryLife > 0)
+                    isTurnedOn = !isTurnedOn;
+                debouncing = true;
+                debounce = 0.0f;
+            }
+
+            if (Input.GetAxis("HeadLampRecharge") > 0.0f)
+            {
+                currentBatteryLife = Mathf.Min(batteryLifeInSeconds, currentBatteryLife + batteryLifeInSeconds / numberOfShakesToCharge);
+                SoundManagerController.Instance.PlaySoundAt((currentBatteryLife == batteryLifeInSeconds) ? rechargeFull : recharge, transform.position);
+                debouncing = true;
+                debounce = 0.0f;
+            }
+        }
+        else if (debouncing)
+        {
+            debounce += Time.deltaTime;
+        }
+        else if (Input.GetAxis("HeadLampToggle") < 0.1f && Input.GetAxis("HeadLampRecharge") > 0.1f)
+        {
+            debouncing = false;
         }
 
         if (isTurnedOn)
@@ -40,12 +65,6 @@ public class HeadLamp : MonoBehaviour {
             }
         }
 
-        if (Input.GetAxis("HeadLampRecharge") > 0.0f)
-        {
-            currentBatteryLife = Mathf.Min(batteryLifeInSeconds, currentBatteryLife + batteryLifeInSeconds / numberOfShakesToCharge);
-            SoundManagerController.Instance.PlaySoundAt( ( currentBatteryLife == batteryLifeInSeconds ) ? rechargeFull : recharge, transform.position);
-        }
-
         if (isTurnedOn)
         {
             float weakenedPower = (currentBatteryLife > weakenTime) ? 1 : currentBatteryLife / weakenTime;
@@ -56,5 +75,6 @@ public class HeadLamp : MonoBehaviour {
         {
             light.enabled = false;
         }
+
 	}
 }
