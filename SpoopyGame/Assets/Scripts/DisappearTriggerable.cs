7e7702e7d2a9f6dcc404b961e46d47e3ceca0c56
Anson Wayman <Kanoyugoro@gmail.com>
2015-01-12 16:54:11 +0000

    spoopy ghost
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@28 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/DisappearTriggerable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisappearTriggerable : Triggerable {
+
+    private bool fading = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fading)
+        {
+            if (renderer.material.color.a > 0)
+            {
+                renderer.material.SetColor("Main Color", new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, renderer.material.color.a - 100 * Time.deltaTime));
+
+            }
+            else
+            {
+                fading = false;
+            }
+        }
+    }
+
+
+    void Triggered()
+    {
+        fading = true;
+    }
+}
