e21d9c1c1f783570ac003fff38e6190d98cf9dc6
Talan Lousignont <Talan.L.Lousignont@gmail.com>
2015-01-12 16:37:08 +0000

    Door and Key
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@26 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    public GameObject TargetKey;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.parent == TargetKey)
+        {
+            Destroy(this);
+        }
+    }
+}
