c6f4520cf39c92023efea649cca348b3de92bedf
Jonathan Pearl <jonathantpearl@gmail.com>
2015-01-13 16:27:32 +0000

    Added cheating
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@45 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/CheatingController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatingController : MonoBehaviour {
+
+    private bool isCheating = false;
+    public GameObject path;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            isCheating = !isCheating;
+        }
+
+        if (isCheating)
+        {
+            GameObject.Find("Player").GetComponent<MentalStability>().insanity = 0;
+            GameObject.Find("Player").GetComponent<HealthyLiving>().health = 1;
+            if (path != null)
+                path.renderer.enabled = true;
+        }
+        else
+        {
+            if (path != null)
+                path.renderer.enabled = false;
+        }
+	}
+}
