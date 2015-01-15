d1215838af9d0acf2476ba43e2f84874e9670a3a
Devon Truman <dtruman@student.neumont.edu>
2015-01-15 17:01:50 +0000

    Some Senses
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@54 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/AI/Senses/Auratic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Auratic : SenseInterface
+{
+    public override Brain.SensedInfo Sense()
+    {
+        Brain.SensedInfo si=new Brain.SensedInfo();
+
+        GameObject player = GameObject.Find("Player");
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= distance && !Physics.Raycast(transform.position, player.transform.position - transform.position, Vector3.Distance(transform.position, player.transform.position)))
+        {
+            si.AlertingFactor = 1.0f;
+            si.CertaintyIsPlayer = 1.0f;
+            si.CertaintyOfDirection = 1.0f;
+            si.CertaintyOfDistance = 1.0f;
+            si.SensedDirection = player.transform.position - transform.position;
+            si.SensedDistance = Vector3.Distance(player.transform.position, transform.position);
+        }
+        else
+        {
+            si.AlertingFactor = 0.0f;
+            si.CertaintyIsPlayer = 0.0f;
+            si.CertaintyOfDirection = 0.0f;
+            si.CertaintyOfDistance = 0.0f;
+            si.SensedDirection = new Vector3(0.0f, 0.0f, 0.0f);
+            si.SensedDistance = 0;
+        }
+
+        return si;
+    }
+}
---
+++ SpoopyGame/Assets/AI/Senses/Sight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sight : SenseInterface
+{
+    public override Brain.SensedInfo Sense()
+    {
+        Brain.SensedInfo si = new Brain.SensedInfo();
+
+        GameObject player = GameObject.Find("Player");
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= distance && !Physics.Raycast(transform.position, player.transform.position - transform.position, Vector3.Distance(transform.position, player.transform.position)) && Vector3.Angle(transform.forward, player.transform.position - transform.position) < 180)
+        {
+            si.AlertingFactor = 1.0f;
+            si.CertaintyIsPlayer = 1.0f;
+            si.CertaintyOfDirection = 1.0f;
+            si.CertaintyOfDistance = 0.8f;
+            si.SensedDirection = player.transform.position - transform.position;
+            si.SensedDistance = Vector3.Distance(player.transform.position, transform.position);
+        }
+        else
+        {
+            si.AlertingFactor = 0.0f;
+        }
+
+        return si;
+    }
+}
