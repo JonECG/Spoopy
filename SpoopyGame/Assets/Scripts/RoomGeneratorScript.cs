116a4cb46c38fbe00feae848fbbedd556248b756
Anson Wayman <Kanoyugoro@gmail.com>
2015-01-20 03:43:22 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@63 7a713aaa-3320-a543-9002-538bd3c5cc84

--- SpoopyGame/Assets/Scripts/RoomGeneratorScript.cs
+++ SpoopyGame/Assets/Scripts/RoomGeneratorScript.cs
@@ -8,13 +8,17 @@ public class RoomGeneratorScript : MonoBehaviour
     public GameObject Corner;
     public GameObject Wall;
     public GameObject DoorWay;
+    public GameObject HallWay;
     public enum Directions{UP,DOWN,LEFT,RIGHT};
     private int tileWidth = 2;
     private int tileHeight = 2;
 	// Use this for initialization
 	void Start () 
     {
-        //getRectRoom(2, 3, true, true, true, true);
+        getRectHallWay(7, 7, true, true, false, false);
+
+        GameObject temp = getRectRoom(7, 7, true, false, false, false);
+        temp.transform.Translate(new Vector3(0, 0, tileWidth * 7));
 	}
 	
 	// Update is called once per frame
@@ -22,6 +26,147 @@ public class RoomGeneratorScript : MonoBehaviour
     {
 	
 	}
+
+    public GameObject getRectHallWay(int XTileWidth, int YTileHeight, bool northDoor, bool eastDoor, bool southDoor, bool westDoor)
+    {
+        GameObject parentRoomObject = new GameObject();
+
+
+        for (int i = 0; i < XTileWidth; i++)
+        {
+            for (int j = 0; j < YTileHeight; j++)
+            {
+                if (i < Mathf.Round(XTileWidth / 2) && j == Mathf.Round(YTileHeight / 2) && eastDoor)
+                {
+                    GameObject temp2 = (GameObject)Instantiate(HallWay);
+                    temp2.transform.parent = parentRoomObject.transform;
+                    temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                }
+                else
+                if (i > Mathf.Round(XTileWidth / 2) && j == Mathf.Round(YTileHeight / 2) && westDoor)
+                {
+                    GameObject temp2 = (GameObject)Instantiate(HallWay);
+                    temp2.transform.parent = parentRoomObject.transform;
+                    temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                }
+                else
+                if (j < Mathf.Round(YTileHeight / 2) && i == Mathf.Round(XTileWidth / 2) && northDoor)
+                {
+                    GameObject temp2 = (GameObject)Instantiate(HallWay);
+                    temp2.transform.parent = parentRoomObject.transform;
+                    temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                    temp2.transform.Rotate(new Vector3(0, 90, 0));
+                }
+                else
+                if (j > Mathf.Round(YTileHeight / 2) && i == Mathf.Round(XTileWidth / 2) && southDoor)
+                {
+                    GameObject temp2 = (GameObject)Instantiate(HallWay);
+                    temp2.transform.parent = parentRoomObject.transform;
+                    temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2),0, j * tileHeight - (YTileHeight / 2)));
+                    temp2.transform.Rotate(new Vector3(0, 90, 0));
+                }
+                else
+                if (i == Mathf.Round(XTileWidth/2) && j == Mathf.Round(YTileHeight/2))
+                {
+                    if (northDoor && southDoor && eastDoor && westDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Floor);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                    }
+                    else
+                    if (northDoor && southDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Wall);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        temp2.transform.Rotate(new Vector3(0, 90, 0));
+                    }
+                    else
+                    if (northDoor && southDoor && westDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Wall);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        temp2.transform.Rotate(new Vector3(0, -90, 0));
+                    }
+                    else
+                    if (northDoor && westDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Wall);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                    }
+                    else
+                    if (southDoor && westDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Wall);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        temp2.transform.Rotate(new Vector3(0, 180, 0));
+                    }
+                    else
+                    if (southDoor && northDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(HallWay);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        //temp2.transform.Rotate(new Vector3(0, 180, 0));
+                        temp2.transform.Rotate(new Vector3(0, 90, 0));
+                    }
+                    else
+                    if (southDoor && westDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Corner);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        //temp2.transform.Rotate(new Vector3(0, 180, 0));
+                        Rotate(4, 0, 4, 0, temp2);
+                    }
+                    else
+                    if (southDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Corner);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        //temp2.transform.Rotate(new Vector3(0, 180, 0));
+                        Rotate(0, 0, 4, 0, temp2);
+                    }
+                    else
+                    if (northDoor && westDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Corner);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        //temp2.transform.Rotate(new Vector3(0, 180, 0));
+                        Rotate(0, 4, 4, 4, temp2);
+                    }
+                    else
+                    if (northDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(Corner);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                        //temp2.transform.Rotate(new Vector3(0, 180, 0));
+                        Rotate(4, 4, 4, 4, temp2);
+                    }
+                    else
+                    if (westDoor && eastDoor)
+                    {
+                        GameObject temp2 = (GameObject)Instantiate(HallWay);
+                        temp2.transform.parent = parentRoomObject.transform;
+                        temp2.transform.Translate(new Vector3(i * tileWidth - (XTileWidth / 2), 0, j * tileHeight - (YTileHeight / 2)));
+                    }
+                }
+
+
+                    
+            }
+        }
+
+
+        return parentRoomObject;
+    }
 
     public GameObject getRectRoom(int XTileWidth, int YTileHeight,bool northDoor, bool eastDoor, bool southDoor, bool westDoor)
     {
