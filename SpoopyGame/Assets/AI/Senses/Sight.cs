fe6c3e4355d925e8ac73fab51e78fafa5b84ef5c
Devon Truman <dtruman@student.neumont.edu>
2015-01-20 02:49:39 +0000

    Fixed sight sense script
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@58 7a713aaa-3320-a543-9002-538bd3c5cc84

--- SpoopyGame/Assets/AI/Senses/Sight.cs
+++ SpoopyGame/Assets/AI/Senses/Sight.cs
@@ -9,14 +9,19 @@ public class Sight : SenseInterface
 
         GameObject player = GameObject.Find("Player");
 
-        if (Vector3.Distance(player.transform.position, transform.position) <= distance && !Physics.Raycast(transform.position, player.transform.position - transform.position, Vector3.Distance(transform.position, player.transform.position)) && Vector3.Angle(transform.forward, player.transform.position - transform.position) <= 90)
+        RaycastHit hit;
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= distance && Vector3.Angle(transform.forward, player.transform.position - transform.position) <= 90 && Physics.Raycast(transform.position+transform.up, player.transform.position - transform.position, out hit, distance))
         {
-            si.AlertingFactor = 1.0f;
-            si.CertaintyIsPlayer = 1.0f;
-            si.CertaintyOfDirection = 1.0f;
-            si.CertaintyOfDistance = 0.8f;
-            si.SensedDirection = player.transform.position - transform.position;
-            si.SensedDistance = Vector3.Distance(player.transform.position, transform.position);
+            if (hit.collider.name == "Player")
+            {
+                si.AlertingFactor = 1.0f;
+                si.CertaintyIsPlayer = 1.0f;
+                si.CertaintyOfDirection = 1.0f;
+                si.CertaintyOfDistance = 0.8f;
+                si.SensedDirection = player.transform.position - transform.position;
+                si.SensedDistance = Vector3.Distance(player.transform.position, transform.position);
+            }
         }
         else
         {
