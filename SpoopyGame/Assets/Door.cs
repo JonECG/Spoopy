fe5b388d656aacc9656725cc1cabcaef62d91b27
Talan Lousignont <Talan.L.Lousignont@gmail.com>
2015-01-24 01:56:04 +0000

    Box and pillar prop, bases of rooms and doors.
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@71 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+    public bool isLocked { get; set; }
+    public Vector3 exitDirection { get; set; }
+    public Vector3 roomCenterDirection { get; set; }
+    public Vector3 position { get; set; }
+    private Room room { get; set; }
+    private Door otherDoor { get; set; }
+    
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void setExitDirection(Vector3 exitDirection) {
+        this.exitDirection = exitDirection;
+    }
+
+    public void setRoom(Room room)
+    {
+        this.room = room;
+    }
+}
---
+++ SpoopyGame/Assets/Room.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+
+
+    public bool isPreGenreatedRoom { get; set; }
+    public GameObject prebuiltRoom { get; set; }
+    public List<Door> doors = new List<Door>();
+
+    private bool hasPlayer { get; set; }
+    private bool endRoom { get; set; }
+    private int sizeX { get; set; }
+    private int sizeZ { get; set; }
+    private int sightWeight { get; set; }
+    
+	// Use this for initialization
+	void Start () 
+    {
+        createRoom(3, 3, this.gameObject.GetComponent<RoomGeneratorScript>());
+        //this.tag = "Room";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (sightWeight <= 1)
+        {
+            this.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    public void createRoom(int x, int z, RoomGeneratorScript roomGen)
+    {
+        setSize(x, z);
+        if(isPreGenreatedRoom)
+        {
+
+        }
+        else 
+        {
+            genreateRoom(roomGen);
+        }
+    }
+
+
+
+    private void setSize(int x, int z) 
+    {
+        sizeX = x;
+        sizeZ = z;
+    }
+
+    private void addDoor(Door newDoor)
+    {
+        doors.Add(newDoor);
+    }
+
+    private void createRoomTrigger()
+    {
+        BoxCollider roomColider = new BoxCollider();
+        roomColider.center = this.transform.position;
+        roomColider.transform.parent = this.transform;
+        roomColider.size = new Vector3(sizeX, 0.0f, sizeZ);
+        roomColider.isTrigger = true;
+    }
+
+    private void genreateRoom(RoomGeneratorScript roomGen)
+    {
+        bool[] exits = new bool[4];
+        if (endRoom)
+        {
+            exits[0] = true;
+        }
+        else
+        {
+            int exitAmount = Random.Range(1, 4);
+            int exitsOpened = 0;
+            while (exitsOpened <= exitAmount)
+            {
+                int exitSelect = Random.Range(0, 4);
+                if (!exits[exitSelect])
+                {
+                    exits[exitSelect] = true;
+                    exitsOpened++;
+                }
+
+            }
+        }
+        GameObject newRoom = roomGen.getRectRoom(sizeX, sizeZ, exits[0], exits[1], exits[2], exits[3]);
+        newRoom.transform.position = new Vector3(-(sizeX / 2), 0.0f, -(sizeZ/2));
+        
+        if (exits[0])
+        {
+            //Door northDoor = new Door();
+            //northDoor.transform.position = new Vector3(1.0f, 0.0f, 0.0f);
+            //northDoor.setRoom(this);
+            //northDoor.setExitDirection(northDoor.transform.position - this.transform.position);
+            //northDoor.transform.parent = this.transform;
+        }
+        if (exits[1])
+        {
+            //Door eastDoor = new Door();
+            //eastDoor.transform.position = new Vector3(0.0f, 0.0f, 1.0f);
+            //eastDoor.setRoom(this);
+            //eastDoor.setExitDirection(eastDoor.transform.position - this.transform.position);
+            //eastDoor.transform.parent = this.transform;
+        }
+        if (exits[2])
+        {
+            //Door southDoor = new Door();
+            //southDoor.transform.position = new Vector3(-1.0f, 0.0f, 0.0f);
+            //southDoor.setRoom(this);
+            //southDoor.setExitDirection(southDoor.transform.position - this.transform.position);
+            //southDoor.transform.parent = this.transform;
+        }
+        if (exits[3])
+        {
+            //Door westDoor = new Door();
+            //westDoor.transform.position = new Vector3(0.0f, 0.0f, -1.0f);
+            //westDoor.setRoom(this);
+            //westDoor.setExitDirection(westDoor.transform.position - this.transform.position);
+            //westDoor.transform.parent = this.transform;
+        }
+        newRoom.transform.parent = this.transform;        
+    }
+}
