ce794de1d7c0ce48a2a2487ad8518afca7b66f20
Jonathan Pearl <jonathantpearl@gmail.com>
2015-01-26 11:42:55 +0000

    Traversals
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@85 7a713aaa-3320-a543-9002-538bd3c5cc84

--- SpoopyGame/Assets/Scripts/RoomTraversal.cs
+++ SpoopyGame/Assets/Scripts/RoomTraversal.cs
@@ -1,26 +1,154 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class DummyDoor
 {
     public DummyDoor connectedDoor;
     public DummyRoom connectedRoom;
-    public int accessibilityMask;
+
+    private int _accessibilityMask;
+    public int AccessibilityMask
+    {
+        get
+        {
+            return _accessibilityMask | connectedDoor._accessibilityMask;
+        }
+        set
+        {
+            _accessibilityMask = value;
+            connectedDoor._accessibilityMask = value;
+        }
+    }
 }
 
 public class DummyRoom
 {
-    List<DummyDoor> doors;
+    public List<DummyDoor> doors = new List<DummyDoor>();
+    public int accessibilityMask;
+    public int mark;
+    public string name;
 }
 
 public class RoomTraversal : MonoBehaviour {
 
+    DummyRoom startRoom;
+
 	void Start () 
 	{
-	
+        int RED_KEY = 1 << 0;
+        int GREEN_KEY = 1 << 1;
+
+        startRoom = new DummyRoom() { name = "start" };
+
+        /*
+          _     _     _
+         [ ]-G-[ ]-R-[_]
+          R     |
+         [S]---[_]
+         
+         */
+        DummyRoom up = new DummyRoom() { name = "up" };
+        DummyRoom right = new DummyRoom() { name = "right" };
+        DummyRoom upRight = new DummyRoom() { name = "upRight" };
+        DummyRoom upRightRight = new DummyRoom() { name = "upRightRight" };
+
+        DummyDoor start2up = helperLinkRooms(startRoom, up);
+        DummyDoor up2upRight = helperLinkRooms(up, upRight);
+        DummyDoor start2right = helperLinkRooms(startRoom, right);
+        DummyDoor right2upRight = helperLinkRooms(right, upRight);
+        DummyDoor upRight2upRightRight = helperLinkRooms(upRight, upRightRight);
+
+        start2up.AccessibilityMask |= RED_KEY;
+        up2upRight.AccessibilityMask |= GREEN_KEY;
+        upRight2upRightRight.AccessibilityMask |= RED_KEY;
+
+        Traverse(startRoom);
 	}
-	
+
+    private void ClearTraversal(DummyRoom room)
+    {
+        if (room.mark != -1)
+        {
+            room.mark = -1;
+            foreach (DummyDoor door in room.doors)
+            {
+                ClearTraversal(door.connectedDoor.connectedRoom);
+            }
+        }
+    }
+
+    public void Traverse(DummyRoom room)
+    {
+        int currentMask = 0;
+        List<int> neededItems = null;
+        List<DummyRoom> accessibleRooms = new List<DummyRoom>();
+        List<DummyRoom> nowAvailableRooms = null;
+        do
+        {
+            ClearTraversal(room);
+            nowAvailableRooms = new List<DummyRoom>();
+            neededItems = new List<int>();
+
+            TraverseHelper(room, currentMask, neededItems, nowAvailableRooms, 0);
+
+            if (neededItems.Count > 0)
+            {
+                List<DummyRoom> newlyAvailableRooms = nowAvailableRooms.Except(accessibleRooms).ToList();
+                int item = neededItems[Random.Range(0, neededItems.Count)];
+                DummyRoom placeRoom = newlyAvailableRooms[Random.Range(0, newlyAvailableRooms.Count)];
+
+                currentMask |= item;
+                Debug.Log("Placed " + item + " in " + placeRoom.name);
+            }
+
+            accessibleRooms = nowAvailableRooms;
+        }
+        while ( neededItems.Count > 0);
+    }
+
+    private void TraverseHelper(DummyRoom room, int currentMask, List<int> neededItems, List<DummyRoom> accessibleRooms, int currentDepth )
+    {
+        if( room.mark > currentDepth || room.mark == -1 )
+        {
+            room.mark = currentDepth;
+            currentDepth++;
+            accessibleRooms.Add(room);
+
+            foreach (DummyDoor door in room.doors)
+            {
+                int needed = (~currentMask)&door.AccessibilityMask;
+                if (needed == 0)
+                {
+                    TraverseHelper(door.connectedDoor.connectedRoom, currentMask, neededItems, accessibleRooms, currentDepth );
+                }
+                else
+                {
+                    neededItems.Add(needed);
+                }
+            }
+        }
+    }
+
+
+
+    DummyDoor helperLinkRooms(DummyRoom a, DummyRoom b)
+    {
+        DummyDoor ab = new DummyDoor();
+        DummyDoor ba = new DummyDoor();
+
+        ab.connectedDoor = ba;
+        ba.connectedDoor = ab;
+        ab.connectedRoom = a;
+        ba.connectedRoom = b;
+
+        a.doors.Add(ab);
+        b.doors.Add(ba);
+
+        return ab;
+    }
+
 	void Update () 
 	{
 	
