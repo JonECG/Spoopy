ee73f78fdeb565ff9e4fbc22a489c6cdd75600c9
Anson Wayman <Kanoyugoro@gmail.com>
2015-01-26 08:17:21 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@81 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/Scripts/ChalkScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChalkScript : MonoBehaviour 
+{
+    private Camera theCamera;
+    public GameObject chalkquad;
+    private float distance = 4.0f;
+    private Vector3 lastPos;
+    private float threshold = 0.002f;
+	// Use this for initialization
+	void Start () 
+    {
+        theCamera = GameObject.Find("LitCamera").camera;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.U))
+        {
+            placeChalk();
+        }
+	}
+
+    void placeChalk()
+    {
+        RaycastHit hit;
+        Physics.Raycast(theCamera.transform.position, theCamera.transform.forward, out hit, distance, 1 << LayerMask.NameToLayer("Map"));
+        if (hit.collider)
+        {
+            Vector3 tempPos = hit.point + hit.normal * Random.Range(0.01f, 0.02f);
+            if ((tempPos - lastPos).sqrMagnitude > threshold)
+            {
+                GameObject t = (GameObject)Instantiate(chalkquad, tempPos, Quaternion.identity);
+                t.transform.LookAt(t.transform.position + (hit.normal * -1));
+                t.transform.SetParent(hit.collider.gameObject.transform.root, true);
+                lastPos = tempPos;
+            }
+        }
+    }
+}
