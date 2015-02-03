08711f61325ce8449e78395c04e92a716cbd714e
Devon Truman <dtruman@student.neumont.edu>
2015-02-03 05:40:47 +0000

    Falling Room Added
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@116 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/FallToTop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallToTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            GameObject transport = GameObject.Find("AtTop");
+
+            other.transform.position = transport.transform.position;
+        }
+    }
+}
