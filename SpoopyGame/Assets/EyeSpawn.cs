36e66c89060b8652cf84820905cb0f7768431e8e
Devon Truman <dtruman@student.neumont.edu>
2015-02-03 01:18:48 +0000

    Eye Ball Prefab
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@110 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/EyeSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeSpawn : MonoBehaviour {
+
+    public GameObject eye;
+    public float xOff;
+    public float yOff;
+    public float zOff;
+    public int numEyes;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i < numEyes; i++)
+        {
+            float x=transform.position.x+(Random.Range(-xOff, xOff));
+            float y=transform.position.y+(Random.Range(-yOff, yOff));
+            float z=transform.position.z+(Random.Range(-zOff, zOff));
+            Instantiate(eye, new Vector3(x, y, z), Quaternion.identity);
+        }
+	}
+}
---
+++ SpoopyGame/Assets/Scripts/EyeLookAt.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeLookAt : MonoBehaviour
+{
+    private float randomY;
+    private float randomX;
+    private float randomZ;
+    bool isUp;
+    bool isWeave;
+    bool isZMove;
+
+    void Start ()
+    {
+        isUp = randomBoolean();
+        isWeave = randomBoolean();
+        isZMove = randomBoolean();
+        randomX = Random.Range(-0.2f, 0.2f);
+        randomY = Random.Range(-0.2f, 0.2f);
+        randomZ=Random.Range(-0.2f,0.2f);
+        transform.position = new Vector3(transform.position.x, transform.position.y + randomY, transform.position.z);
+        transform.position = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + randomZ);
+    }
+
+    bool randomBoolean()
+    {
+        if (Random.value >= 0.5)
+            return true;
+
+        return false;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject player = GameObject.Find("LitCamera");
+        transform.LookAt(player.transform.position-new Vector3(0.0f,0.5f,0.0f));
+
+        if (randomY >= 0.2)
+        {
+            isUp = false;
+        }
+        else if (randomY <= -0.2)
+        {
+            isUp = true;
+        }
+
+        if (randomX >= 0.2)
+        {
+            isWeave = false;
+        }
+        else if (randomX <= -0.2)
+        {
+            isWeave = true;
+        }
+
+        if (randomZ >= 0.2f)
+        {
+            isZMove = false;
+        }
+        else if (randomZ <= -0.2f)
+        {
+            isZMove = true;
+        }
+
+        if (isUp)
+        {
+            transform.position += new Vector3(0, 0.001f, 0);
+            randomY += 0.001f;
+        }
+        else
+        {
+            transform.position -= new Vector3(0, 0.001f, 0);
+            randomY -= 0.001f;
+        }
+
+        if (isWeave)
+        {
+            transform.position += new Vector3(0.001f, 0, 0);
+            randomX += 0.001f;
+        }
+        else
+        {
+            transform.position -= new Vector3(0.001f, 0, 0);
+            randomX -= 0.001f;
+        }
+
+        if (isZMove)
+        {
+            transform.position += new Vector3(0, 0, 0.001f);
+            randomZ += 0.001f;
+        }
+        else
+        {
+            transform.position -= new Vector3(0, 0, 0.001f);
+            randomZ -= 0.001f;
+        }
+	}
+}
