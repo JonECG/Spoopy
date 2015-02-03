5c3ace3c40452ecffa0119aec4f55f5cc08567e9
Jonathan Pearl <jonathantpearl@gmail.com>
2015-02-03 16:22:05 +0000

    Added title screen and made first level more responsive
    git-svn-id: https://svn.neumont.edu:8443/svn/spoopygroup@120 7a713aaa-3320-a543-9002-538bd3c5cc84

---
+++ SpoopyGame/Assets/TitleScreenController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour {
+
+    public GameObject oculCamera, normalCamera;
+    public Texture2D one, two;
+
+    private float waitStarted;
+    public float waitTime = 5;
+
+    Debouncer.DebouncerResults pressACorrected;
+
+	void Start () 
+	{
+        renderer.material.mainTexture = one;
+        waitStarted = 0;
+        if (Application.isEditor)
+        {
+            oculCamera.SetActive(false);
+            normalCamera.SetActive(true);
+        }
+        else
+        {
+            oculCamera.SetActive(true);
+            normalCamera.SetActive(false);
+        }
+	}
+	
+	void Update () 
+	{
+        pressACorrected = Debouncer.Debounce("Interact", pressACorrected);
+        if (waitStarted == 0)
+        {
+            if (pressACorrected.IsPressed())
+            {
+                waitStarted = Time.time;
+                renderer.material.mainTexture = two;
+            }
+        }
+        else
+        {
+            if (waitStarted < Time.time - waitTime)
+            {
+                Application.LoadLevel(1);
+            }
+        }
+	}
+}
